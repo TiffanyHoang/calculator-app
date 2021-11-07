7a41f8895e0c14fa7d2baac6433e86bf4123067c
Tiffany Hoang <tiffany.hoang@myob.com>
2021-11-07 15:22:35 +1100

    add calculator layout & state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState("");
+  const [state, setState] = useState([]);
+
+  const allValue = state.map((s) => s.item);
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+    addToCalc(e.target.value);
+    setValue("");
+  };
+
+  const getResult = (e) => {};
+
+  const addToCalc = (value) => {
+    const newState = { item: value, key: nanoid() };
+    setState([...state, newState]);
+  };
+
+  // TODO:
+  // - Only get last operator
+  // - Clear Button
+  // - GetResult
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Calculator</h1>
+      <div className="result-panel">{allValue}</div>
+      <div className="number-panel">
+        <form onSubmit={getResult}>
+          <button onClick={handleOnClick} value="0">
+            0
+          </button>
+          <button onClick={handleOnClick} value="1">
+            1
+          </button>
+          <button onClick={handleOnClick} value="2">
+            2
+          </button>
+          <button onClick={handleOnClick} value="3">
+            3
+          </button>
+          <button onClick={handleOnClick} value="4">
+            4
+          </button>
+          <button onClick={handleOnClick} value="5">
+            5
+          </button>
+          <button onClick={handleOnClick} value="6">
+            6
+          </button>
+          <button onClick={handleOnClick} value="7">
+            7
+          </button>
+          <button onClick={handleOnClick} value="8">
+            8
+          </button>
+          <button onClick={handleOnClick} value="9">
+            9
+          </button>
+          <button onClick={handleOnClick} value="+">
+            +
+          </button>
+          <button onClick={handleOnClick} value="-">
+            -
+          </button>
+          <button onClick={handleOnClick} value="x">
+            x
+          </button>
+          <button onClick={handleOnClick} value=":">
+            :
+          </button>
+          <button type="submit">=</button>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
